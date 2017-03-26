6934b4ce376e9a574347465177e00d5309110d14
dglez <danielg68@gmail.com>
2017-03-26 11:45:47 -0400

    up vote

--- public/js/client.app.js
+++ public/js/client.app.js
@@ -1,30 +1,29 @@
 /**
  * Created by DG on 3/25/17.
  */
+var body = $('body');
 
+
+
+/* Main*/
 /*Load page */
-
 loadPage();
 
-
+/**********************************************************************************************************************/
 
 
 
 //prepare URL to get Zip
-var prepare_api_url = function(latitude, longitude){
+function prepare_api_url(latitude, longitude){
     var api_key = "AIzaSyCEPHkBSiLOrKJZ3_K6Ndwi6Ofx_2haTm4";
     var api_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
     return api_url + latitude + ',' + longitude + "&key=" + api_key;
 }
 
+/**********************************************************************************************************************/
 
 
-
-
-
-
-
-
+/*Load Page */
 function loadPage() {
     navigator.geolocation.getCurrentPosition(function(pos){
         var lat = pos.coords.latitude;
@@ -34,19 +33,7 @@ function loadPage() {
 
     });
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
+/**********************************************************************************************************************/
 
 
 //GET Event from the Form with Zip code
@@ -55,12 +42,9 @@ $(".form-inline").submit(function(e){
 
     $("#events").html('');
     var zip = $("#zip").val();
-    console.log(zip);
     getEevents(zip);
 });
-
-
-
+/**********************************************************************************************************************/
 
 
 //Get zip to use
@@ -87,6 +71,9 @@ function getZip(lat, lon, callback) {
 
     });
 }
+/**********************************************************************************************************************/
+
+
 
 //Appends a list of events to the DOM
 function getEevents(zip) {
@@ -98,18 +85,46 @@ function getEevents(zip) {
             var li = "<li class=\'list-group-item\' id=\'"+ api_res[i]._id +
                 "\'> <div class=\'col-xs-6\'>" + api_res[i].name +
                 "</div><div class=\'col-xs-offset-2 col-xs-1\'>" +
-                "<span class=\'glyphicon glyphicon-thumbs-up \' aria-hidden=\'true\'>" +
+                "<span class=\'glyphicon glyphicon-thumbs-up active-thumb \' aria-hidden=\'true\'>" +
                 "</span></div><div class=\'col-xs-1\'>" + api_res[i].upVote +
                 "</div><div class=\'col-xs-1\'>" +
-                "<span class=\'glyphicon glyphicon-thumbs-down\' aria-hidden=\'true\'></span>" +
+                "<span class=\'glyphicon glyphicon-thumbs-down active-thumb\' aria-hidden=\'true\'></span>" +
                 "</div><div class=\'col-xs-1\'>" + api_res[i].downVote +"</div>" + "</li>";
 
-
-
             $('#events').append(li);
-
-            console.log(li);
         }
 
     });
 }
+
+
+
+
+/**********************************************************************************************************************/
+
+/*UPDATE LIKES*/
+body.on('click','.glyphicon-thumbs-up', function () {
+    var thumb_up = $(this);
+    var score = thumb_up.parent().next();
+    var id_upvote = thumb_up.parent().parent().attr('id');
+    var api_key = "api/events/upvote";
+
+    $.ajax({
+        url: api_key,
+        method: "POST",
+        data: {id: id_upvote},
+        success: function (data) {
+
+            if(data.error === null){
+                score.text(data.score);
+            }
+        }
+
+
+
+
+
+    });
+
+
+});
